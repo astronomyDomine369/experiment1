cab3fe25c4acc16e9fad5b2b35bb9a3b0164274e
astronomyDomine369 <73468712+astronomyDomine369@users.noreply.github.com>
2021-01-22 17:48:00 +0700

    Add files via upload

---
+++ test/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import './App.css';
+import './tictactoe.css'
+import tictactoe from './tictactoe/tictactoe'
+
+
+//Latihan (1)
+//stateLessComponent
+
+// const Card = (props) => {
+//   return (
+//     <div className='card'>
+//       <h1 className='title'>{props.name}</h1>
+//   <p>{props.job}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Card name='mrdavinci' job='no job'/>
+//       <Card name='elliot alderson' job='hacker'/>
+//       <Card name='sacha baron cohen' job='comedian'/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+//latihan(2)
+//stateFullComponent
+
+// class stateFullComponent extends React.Component {
+//   render() {
+//     return <p>hellowwwww:/</p>
+//   }
+// }
+
+// export default stateFullComponent
+
